133d6d47b132b4dcc2a540fe8e3647c5b9e94025
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-10-29 03:45:28 +0400

    update rehim.py

--- rehim.py
+++ rehim.py
@@ -2,7 +2,8 @@
 #pyrogram
 
 from pyrogram import Client, filters
-
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from Config import Config
 
@@ -18,6 +19,27 @@ rehim = Client(":memory:", api_id, api_hash, bot_token=bot_token)
 def start_command(client, message):
     message.reply_text("Merhaba! Benim adım StartBot. Nasıl yardımcı olabilirim?")
     
+    
+# Komutları tanımlayın
+@rehim.on_message(filters.command(["chatbot"]))
+def start(_, message):
+    markup = InlineKeyboardMarkup([[InlineKeyboardButton("Aktiv Et", callback_data="activate"),
+                                   InlineKeyboardButton("Deaktiv Et", callback_data="deactivate")]])
+    message.reply_text("Bot şu an deaktif.", reply_markup=markup)
+
+@rehim.on_callback_query()
+def button(_, callback):
+    if callback.data == "activate":
+        callback.message.reply_text("Bot aktiv edildi.")
+        callback.message.edit_reply_markup(reply_markup=None)
+    elif callback.data == "deactivate":
+        callback.message.reply_text("Bot deaktiv edildi.")
+        callback.message.edit_reply_markup(reply_markup=None)
+
+@rehim.on_message(filters.text & ~filters.command)
+def echo(_, message):
+    if message.text.lower() == "selam":
+        message.reply_text("Aleyküm Selam!")
 #TELETHON SETRİ
 
 
