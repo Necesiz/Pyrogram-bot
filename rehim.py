8d308cf275aaf791346356c620b20bbc5d1fc39c
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-10-29 05:11:07 +0400

    update rehim.py

--- rehim.py
+++ rehim.py
@@ -1,7 +1,6 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-#pyrogram
-
-#ss
 from Config import Config
 
 api_id = Config.API_ID
@@ -9,30 +8,43 @@ api_hash = Config.API_HASH
 bot_token = Config.BOT_TOKEN
 
 
-from telethon import TelegramClient, events
 
-client = TelegramClient('bot_session', api_id, api_hash).start(bot_token=bot_token)
+app = TelegramClient('bot_session', api_id, api_hash).start(bot_token=bot_token)
+def get_chat_info(chat):
+    chat_name = chat.title
+    messages_count = chat.total_messages
+    return chat_name, messages_count
 
-@client.on(events.NewMessage(pattern='/purge'))
-async def purge_messages(event):
-    if event.is_private:
-        await event.respond("Bu komut gruplarda kullanılmalıdır.")
-        return
+def get_user_info(user):
+    user_id = user.id
+    first_name = user.first_name
+    last_name = user.last_name
+    return user_id, first_name, last_name
 
-    # Yanıtlanan mesajın bilgilerini al
-    replied_msg = await event.get_reply_message()
-    if not replied_msg:
-        await event.respond("Lütfen bu komutu yanıtlanan bir mesaj üzerinde kullanın.")
-        return
+@app.on_message(filters.command("me"))
+def my_info(_, message):
+    chat_name, messages_count = get_chat_info(message.chat)
+    user_id, first_name, last_name = get_user_info(message.from_user)
 
-    # Yanıtlanan mesajın altındaki tüm mesajları sil
-    chat = await event.get_chat()
-    messages = await client.get_messages(chat, reply_to=replied_msg.id)
-    await client.delete_messages(chat, messages)
+    reply_markup = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Məlumat al", callback_data=f"get_info_{user_id}")]
+    ])
 
-    # Kimin tarafından yapıldığını ve kaç saniyede silindiğini kaydet
-    admin = await client.get_entity(event.sender_id)
-    await event.respond(f"{len(messages)} mesaj {admin.first_name} tarafından {event.date - replied_msg.date} saniyede silindi.")
+    message.reply_text(f"Chat adı: {chat_name}\nMesaj sayısı: {messages_count}\n"
+                       f"Kullanıcı ID: {user_id}\nAd: {first_name}\nSoyad: {last_name}", 
+                       reply_markup=reply_markup)
 
-client.run_until_disconnected()
+@app.on_callback_query()
+def get_info(_, query):
+    user_id = int(query.data.split("_")[2])
+    user = app.get_users(user_id)
+    _, _, _ = get_user_info(user)
+    query.answer(f"Kullanıcı ID: {user_id}\nAd: {user.first_name}\nSoyad: {user.last_name}")
+
+app.run()
+
+#pyrogram
+
+#ss
+
 
