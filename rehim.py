e40039d92cbb6c3903b725ecbd804e57ba4e7406
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-10-29 04:33:22 +0400

    update rehim.py

--- rehim.py
+++ rehim.py
@@ -12,37 +12,30 @@ api_hash = Config.API_HASH
 bot_token = Config.BOT_TOKEN
 
 
+from telethon import TelegramClient, events
 
-rehim = Client(":memory:", api_id, api_hash, bot_token=bot_token)
+client = TelegramClient('bot_session', api_id, api_hash).start(bot_token=bot_token)
 
-@rehim.on_message(filters.command(['start']))
-def start_command(client, message):
-    message.reply_text("Merhaba! Benim adım StartBot. Nasıl yardımcı olabilirim?")
-    
-    
-# Komutları tanımlayın
-@rehim.on_message(filters.command(["chatbot"]))
-def start(_, message):
-    markup = InlineKeyboardMarkup([[InlineKeyboardButton("Aktiv Et", callback_data="activate"),
-                                   InlineKeyboardButton("Deaktiv Et", callback_data="deactivate")]])
-    message.reply_text("Bot şu an deaktif.", reply_markup=markup)
+@client.on(events.NewMessage(pattern='/purge'))
+async def purge_messages(event):
+    if event.is_private:
+        await event.respond("Bu komut gruplarda kullanılmalıdır.")
+        return
 
-@rehim.on_callback_query()
-def button(_, callback):
-    if callback.data == "activate":
-        callback.message.reply_text("Bot aktiv edildi.")
-        callback.message.edit_reply_markup(reply_markup=None)
-    elif callback.data == "deactivate":
-        callback.message.reply_text("Bot deaktiv edildi.")
-        callback.message.edit_reply_markup(reply_markup=None)
+    # Yanıtlanan mesajın bilgilerini al
+    replied_msg = await event.get_reply_message()
+    if not replied_msg:
+        await event.respond("Lütfen bu komutu yanıtlanan bir mesaj üzerinde kullanın.")
+        return
 
-@rehim.on_message(filters.text & filters.command)
-def echo(_, message):
-    if message.text() == "selam":
-        message.reply_text("Aleyküm Selam!")
-#TELETHON SETRİ
+    # Yanıtlanan mesajın altındaki tüm mesajları sil
+    chat = await event.get_chat()
+    messages = await client.get_messages(chat, reply_to=replied_msg.id)
+    await client.delete_messages(chat, messages)
 
+    # Kimin tarafından yapıldığını ve kaç saniyede silindiğini kaydet
+    admin = await client.get_entity(event.sender_id)
+    await event.respond(f"{len(messages)} mesaj {admin.first_name} tarafından {event.date - replied_msg.date} saniyede silindi.")
 
-
-rehim.run()
+client.run_until_disconnected()
 
