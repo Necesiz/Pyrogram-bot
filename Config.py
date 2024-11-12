d752bdd565b98d534e3b10ad12d0e429f58028a3
Necesiz <97000998+Necesiz@users.noreply.github.com>
2024-11-12 18:37:36 +0400

    Update Config.py

--- Config.py
+++ Config.py
@@ -9,3 +9,4 @@ class Config():
     API_HASH = os.environ.get("API_HASH","85adea6f1eaf068b707703b4846a9ced")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5350952995:AAE1Z6Z2E1Va-axYooqcz4qexFbfeTQ0rpU")
     
+6455035511:AAEyXQTsFB-yCG601G_1dwiv4A7Xf0w9Cok
